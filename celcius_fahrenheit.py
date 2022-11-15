2cc32aa91b896d3fc919e84e241e42a156ba9c75
santoshnainala <51361959+santoshnainala@users.noreply.github.com>
2022-11-15 18:08:07 +0530

    Add files via upload

---
+++ celcius_fahrenheit.py
@@ -0,0 +1,6 @@
+#Converts celcius to fahrenheit
+celsius = float(input("Enter the celsius value : "))
+
+# calculate fahrenheit
+fahrenheit = (celsius * 1.8) + 32
+print(celsius,"degree celsius in fahrenheit is :",fahrenheit,"degree fahrenheit")
